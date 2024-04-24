85e54e95abe656ccaef8571e522065cde35c4f6c
sai jagadish nukala <saijagadish.nukala@gmail.com>
2024-04-24 19:06:41 +0530

    Frontend project with React Js

---
+++ frontend/myreactapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { Simple } from './components/Simple';
+
+function App() {
+  return (
+    <div><h2>This is the root component</h2><Simple></Simple></div>
+
+  );
+}
+
+export default App;
---
+++ frontend/myreactapp/src/components/Simple.js
@@ -0,0 +1,4 @@
+export function Simple(){
+    let username='Sai Jagadish';
+    return(<div><h3>Hello {username}</h3></div>)
+}
